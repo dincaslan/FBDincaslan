100678edf08c2a572de411365a36689c486d8fcf
dincaslan <fatmab.dincaslan@gmail.com>
2024-01-12 16:20:12 +0300

    site

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    // For scrolling
+    const links = document.querySelectorAll('a]href^="#"}') ;
+    links.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            window.scrollTo({
+                top: targetElement.offsetTop - 50,
+                beahaviour: 'smooth'
+            
+            });
+        });
+    });
+}) ;
